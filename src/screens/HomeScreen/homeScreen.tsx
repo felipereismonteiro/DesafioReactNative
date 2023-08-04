910e2b594fe31f0798ac33c343ca8cc08ffddc94
felipereismonteiro <felipe.dreis.monteiro@gmail.com>
2023-08-04 16:15:17 -0300

    feat: mudança de input ao pesquisar o usuário.

--- src/screens/HomeScreen/homeScreen.tsx
+++ src/screens/HomeScreen/homeScreen.tsx
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
 import { GithubRoute } from "@/api/GithubRoute";
 import { UserModel } from "@/model/userGithub.model";
+import InputUserInfo from "./components/inputUserInfo";
 import styled from "styled-components/native";
-import FontAwesome5Icon from "@/components/fontAwesome5.icon";
-import { Formik } from "formik";
-import IonIcon from "@/components/ionIcon.icon";
-import ErrorMessage from "@/components/errorMessage";
 
 export default function HomeScreen({ navigation }: any) {
   const [userInfos, setUserInfos] = useState<UserModel | undefined>();
   const [error, setError] = useState<boolean>(false);
+  console.log(userInfos ? true : false);
 
   const handleFindUser = async ({ userName }: { userName: string }) => {
-    const usernameRegex = /^(?!.*(--|-{2,})|.*-$|.*-0)(?!.*github)[a-zA-Z0-9-]{1,39}$/;
+    const usernameRegex =
+      /^(?!.*(--|-{2,})|.*-$|.*-0)(?!.*github)[a-zA-Z0-9-]{1,39}$/;
     const isValid = usernameRegex.test(userName);
     if (!isValid) {
       setError(true);
-      setTimeout(() => { setError(false) }, 5000)
+      setTimeout(() => {
+        setError(false);
+      }, 5000);
       return;
     }
 
@@ -31,76 +32,16 @@ export default function HomeScreen({ navigation }: any) {
   };
 
   return (
-    <Container>
-      <Formik
-        initialValues={{
-          userName: "",
-        }}
-        onSubmit={handleFindUser}
-      >
-        {({ handleChange, handleSubmit, values }) => (
-          <InputContainer>
-            <ContainerSearch>
-              <FontAwesome5Icon name="search" size={15} />
-            </ContainerSearch>
-            <ContainerSubmit>
-              <SendButton
-                onPress={handleSubmit}
-                title={<IonIcon name="send" size={15} />}
-              />
-            </ContainerSubmit>
-            <SearchUser
-              onChangeText={handleChange("userName")}
-              placeholder="Pesquisar Usuário"
-              value={values.userName}
-            />
-          </InputContainer>
-        )}
-      </Formik>
-      {error && <ErrorMessage />}
-    </Container>
+    <>
+        <InputUserInfo handleFindUser={handleFindUser} error={error} topInput={userInfos ? true : false}/>
+      
+    </>
   );
 }
 
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-
-const InputContainer = styled.View`
-  position: relative;
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ContainerSearch = styled.View`
+const ContainerInput = styled.View`
   position: absolute;
-  left: 10px;
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ContainerSubmit = styled.View`
-  position: absolute;
-  right: 5px;
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-
-const SearchUser = styled.TextInput`
-  width: 60vw;
-  max-width: 300px;
-  border: 1px solid black;
-  border-radius: 5px;
-  padding: 10px 17% 10px 11%;
-  text-align: center;
-`;
-
-const SendButton = styled.Button`
-  border-radius: 50%;
-  margin-right: -20px;
-`;
+  top: 20px;
+  right: 0px;
+  margin: 10px;
+`
